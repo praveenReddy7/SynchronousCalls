22e5de07735bd6bdf113ead461bdc8b63b0c9045
praveen <v.praveen@centrahub.com>
2019-10-28 12:14:25 +0530

    Initial Commit

---
+++ SynchronousCall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SynchronousCall
+//
+//  Created by praveen on 10/28/19.
+//  Copyright © 2019 focussoftnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
